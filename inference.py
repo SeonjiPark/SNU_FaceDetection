37997140faf039a889a8fb5acac9d1dfc4bf22fd
hochang <myideaisgood@hanmail.net>
2022-07-26 15:25:51 +0900

    Fix
    Fix

--- inference.py
+++ inference.py
@@ -183,7 +183,7 @@ def inference(args):
             img_name = img_name.split('/')
             img_name = img_name[-1]
 
-            cv2.imwrite(SAVE_FOLDER + 'result_images/' +img_name, img_raw)
+            cv2.imwrite(SAVE_FOLDER + 'result_images/' + img_name, img_raw)
 
         avg_ftime += f_ftime
         avg_dtime += f_dtime
@@ -239,7 +239,7 @@ def inference(args):
                     if SAVE_MASK:
                         original_img_name, original_img_format = img_name.split(".")
                         cv2.imwrite(os.path.join(SAVE_FOLDER, 'head_masks', original_img_name + '_' + str(idx) + '.' + original_img_format), mask)
-                        cv2.imwrite(os.path.join(SAVE_FOLDER, 'heads', original_img_name + '_' + str(idx) + '.' + original_img_format), head)
+                        cv2.imwrite(os.path.join(SAVE_FOLDER, 'faces', original_img_name + '_' + str(idx) + '.' + original_img_format), head)
                         cv2.imwrite(os.path.join(SAVE_FOLDER, 'masks', original_img_name + '_' + str(idx) + '.' + original_img_format), mask[CY-H:CY+H, CX-W:CX+W, :])
 
 
